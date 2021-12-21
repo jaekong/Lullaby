d04c158c439e7207a84f45bf5821a2a1c7f86534
Jae Kong <jtodaone@icloud.com>
2021-12-22 04:15:07 +0900

    fix: enable unsafe flag only on macOS

--- Package.swift
+++ Package.swift
@@ -3,36 +3,34 @@
 
 import PackageDescription
 
+#if os(macOS)
 let package = Package(
     name: "Lullaby",
     platforms: [
         .macOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Lullaby",
             targets: ["Lullaby"]),
+        .library(
+            name: "LullabyMusic",
+            targets: ["LullabyMusic"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/thara/SoundIO.git", from: "0.3.2"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "0.0.5"))
-//        .package(url: "https://github.com/SammySmallman/OSCKit", .upToNextMajor(from: "3.0.1"))
 //        .package(url: "https://github.com/apple/swift-numerics.git", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Lullaby",
             dependencies: [
                 .product(name: "SoundIO", package: "SoundIO"),
                 .product(name: "Collections", package: "swift-collections"),
                 .target(name: "LullabyMusic")
-            ]
-//            linkerSettings: [.unsafeFlags(["-L/usr/local/lib"])]
+            ],
+            linkerSettings: [.unsafeFlags(["-L/usr/local/lib"])]
         ),
         .target(
             name: "LullabyMusic"
@@ -41,7 +39,42 @@ let package = Package(
             name: "LullabyTests",
             dependencies: [
                 .target(name: "Lullaby")
-//                .product(name: "OSCKit", package: "OSCKit")
-            ]),
+            ])
     ]
 )
+#else
+let package = Package(
+    name: "Lullaby",
+    products: [
+        .library(
+            name: "Lullaby",
+            targets: ["Lullaby"]),
+        .library(
+            name: "LullabyMusic",
+            targets: ["LullabyMusic"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/thara/SoundIO.git", from: "0.3.2"),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "0.0.5"))
+//        .package(url: "https://github.com/apple/swift-numerics.git", .upToNextMajor(from: "0.1.0"))
+    ],
+    targets: [
+        .target(
+            name: "Lullaby",
+            dependencies: [
+                .product(name: "SoundIO", package: "SoundIO"),
+                .product(name: "Collections", package: "swift-collections"),
+                .target(name: "LullabyMusic")
+            ]
+        ),
+        .target(
+            name: "LullabyMusic"
+        ),
+        .testTarget(
+            name: "LullabyTests",
+            dependencies: [
+                .target(name: "Lullaby")
+            ])
+    ]
+)
+#endif
