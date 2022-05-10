6af22fa2e1bf2bbb7cd84357bf4ea2e51da8815d
Jae Kong <jtodaone@icloud.com>
2022-05-11 04:08:50 +0900

    feat: add ADSR envelope function

--- Sources/Lullaby/Envelope/ADSR.swift
+++ Sources/Lullaby/Envelope/ADSR.swift
@@ -1,3 +1,52 @@
 import Foundation
 
-
+public func adsr(trigger: Signal, attack: Time, decay: Time, sustain: Amplitude, release: Time) -> Signal {
+    var internalTriggeredTime: Time? = nil
+    var internalReleasedTime: Time? = nil
+    
+    precondition(attack >= 0, "Attack value should be positive.")
+    precondition(decay >= 0, "Decay value should be positive.")
+    precondition(sustain >= 0, "Sustain value should be positive.")
+    precondition(release >= 0, "Release value should be positive.")
+    
+    return Signal({ time -> Sample in
+        let triggerSample = trigger(time)
+        if internalTriggeredTime == nil && triggerSample >= 0.5 {
+            print("triggered at \(time)")
+            internalTriggeredTime = time
+            return 0
+        }
+        
+        guard let triggeredTime = internalTriggeredTime else {
+            return 0
+        }
+        
+        if internalReleasedTime == nil && triggerSample < 0.5 {
+            print("released at \(time)")
+            internalReleasedTime = time
+            return sustain
+        }
+        
+        let elapsedTime = time - triggeredTime
+        
+        switch elapsedTime {
+        case ..<attack:
+            return elapsedTime / attack
+        case ..<(attack + decay):
+            return 1 - ((1 - sustain) / decay) * (elapsedTime - attack)
+        case (attack + decay)...:
+            guard let releasedTime = internalReleasedTime else {
+                return sustain
+            }
+            
+            if releasedTime + release <= time {
+                internalTriggeredTime = nil
+                internalReleasedTime = nil
+            }
+            
+            return sustain - (sustain * (time - releasedTime) / release)
+        default:
+            return 0
+        }
+    })
+}
--- Tests/LullabyTests/LullabyTests.swift
+++ Tests/LullabyTests/LullabyTests.swift
@@ -28,6 +28,39 @@ final class LullabyTestsMiniAudio: XCTestCase {
         try engine.stop()
     }
     
+    func testADSR() async throws {
+        let value = Value(value: 440)
+
+        let modulator: Signal = await sine(frequency: value.output) * ((sine(frequency: value.output) + 1) * 250) + value.output
+        let carrier = sine(frequency: modulator)
+        
+        let trigger = Value(value: 0)
+        
+        let envelope = adsr(trigger: await trigger.output, attack: 0.1, decay: 0.5, sustain: 0.5, release: 0.5)
+        
+        let task = Task {
+            for i in twelveToneEqualTemperamentTuning.pitches {
+                await value.setValue(Sample(i * 440))
+                await trigger.setValue(1)
+                print(await trigger.value)
+                await Task.sleep(seconds: 2)
+                await trigger.setValue(0)
+                print(await trigger.value)
+                await Task.sleep(seconds: 2)
+            }
+        }
+
+        let engine = try await MiniAudioEngine()
+
+        engine.setOutput(to: carrier * envelope)
+        try engine.prepare()
+        try engine.start()
+        
+        await task.value
+        
+        try engine.stop()
+    }
+    
     func testFM() async throws {
         let value = Value(value: 440)
 
