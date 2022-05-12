57cd779225777492765d45053407ac40d359a743
Jae Kong <jtodaone@icloud.com>
2022-05-13 02:45:51 +0900

    fix: change Phase typealias to match Precision

--- Sources/Lullaby/Wave/Wave.swift
+++ Sources/Lullaby/Wave/Wave.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public typealias Phase = Double
+public typealias Phase = Precision
 
 /// A type of function that recieves value in 0...1 and returns sample value.
 public typealias Wave = (Phase) -> Sample
