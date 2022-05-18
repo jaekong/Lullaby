efb8938b7ec048e5ce6bf5083cae85705f667877
Jae Kong <jtodaone@icloud.com>
2022-05-18 21:14:59 +0900

    docs: add documentation comment to Phase typealias

--- Sources/Lullaby/Wave/Wave.swift
+++ Sources/Lullaby/Wave/Wave.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// A number in 0...1 range that represents a phase value.
 public typealias Phase = Precision
 
 /// A type of function that recieves value in 0...1 and returns sample value.
